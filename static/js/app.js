8b9e13267a64eee4fa6fa4324c5eafebf66de7bc
jzhupan <jzhupan@gmail.com>
2022-12-02 21:38:48 -0500

    Added contact form, implemented simple form validation with javascript on contact page, tested zoom hover effects on a variety of elements with css.

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,33 @@
-console.log("hi");
+function triggerAlert() {
+  alert("Your form has been submitted, Pink Du will reach out ASAP!");
+}
+
+function blurFunction(inputText) {
+  let emailValidators = ["@hotmail.com", "@gmail.com", "@yahoo.com"];
+
+  if (inputText.id === "firstName") {
+    if (inputText.value === "") {
+      inputText.style.background = "rgba(255,0,0.9,0.5)";
+    } else {
+      inputText.style.background = "none";
+    }
+  } else if (inputText.id === "email") {
+    if (inputText.value === "") {
+      inputText.style.background = "rgba(255,0,0.9,0.5)";
+    } else {
+      let valid = false;
+      for (let i = 0; i < emailValidators.length; i++) {
+        if (inputText.value.includes(emailValidators[i])) {
+          valid = true;
+        }
+      }
+      if (valid) {
+        inputText.style.background = "none";
+      } else {
+        inputText.style.background = "rgba(255,0,0.9,0.5)";
+      }
+    }
+  }
+}
+
+// inputText.style.background = "rgba(255,0,0.9,0.5)";
